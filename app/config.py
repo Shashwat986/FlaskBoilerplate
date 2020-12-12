311483eeabc6061ad8fe7adae677747a6e7d7dd2
Shashwat Chandra <chandra.shashwat@gmail.com>
2020-12-12 16:06:00 +0530

    Adding supervisor + Moving all DB files to /tmp

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 config = {
-    "SQLALCHEMY_DATABASE_URI": "sqlite:///test.db",
+    "SQLALCHEMY_DATABASE_URI": "sqlite:///../tmp/test.db",
     "REDIS_URL": "redis://localhost:6379/0",
     "HONEYBADGER_API_KEY": "4c6df04e"
 }
